59f1790a6c7455755cf35fd89a27728349066cf4
Yarith Joseph <yarithjoseph@gmail.com>
2016-10-14 12:22:17 +0200

    app done

--- app.js
+++ app.js
@@ -1,15 +1,70 @@
 // importing necessary modules
 const fs = require ('fs')
 
+//helper function
+
+var roundDecimal = (number) => {
+		return Math.round(number * 100) / 100
+	}
+
+var addCommas = (number ) => {
+	  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+let prettyNr = (number) => {
+	return addCommas( roundDecimal(number))
+}
+
 //read the customer data json 
 fs.readFile(__dirname + "/customer.json", "utf8", (err,data)=>{
 	//parse the file to a readable object
 	let parseData = JSON.parse(data)
 	console.log(parseData)
+	calcCompound(parseData)
 })
-
 
 // old syntax
 // fs.readFile(__dirname + "/customer.json", "utf8", function (err,date) {
-	
 // })
+
+// function to calculate compound interest from a customer object
+
+var calcCompound = (customer) => {
+
+	// set end amount prop and calculate total duration
+
+	customer.pension.endamount = {
+
+		pessimistic: customer.finances.startcapital,
+		average: customer.finances.startcapital,
+		optimistic: customer.finances.startcapital
+	}
+	customer.pension.duration = (customer.pension.age - customer.age )
+	
+	// do the interest math
+	for (var i = customer.pension.duration - 1; i >= 0; i--) {
+			//add monthly spend to all the scenario's
+			customer.pension.endamount.pessimistic += 	(customer.finances.monthlyadd * 12)
+			customer.pension.endamount.average += 		(customer.finances.monthlyadd * 12)
+			customer.pension.endamount.optimistic += 	(customer.finances.monthlyadd * 12)
+			//calculate the added interest
+			customer.pension.endamount.pessimistic *= 	customer.pension.interest.pessimistic
+			customer.pension.endamount.average *= 		customer.pension.interest.average
+			customer.pension.endamount.optimistic *= 	customer.pension.interest.optimistic
+
+
+		
+		}
+			//output our data
+			console.log ("welcome " + customer.name + " to our advanced pension planner")
+			console.log ("you are starting with " + customer.finances.startcapital + " and add a monthly amound of " + customer.finances.monthlyadd)
+			console.log("when you retire at age " + customer.pension.age + " you will have the following ")
+			//output calulation stuff
+			console.log ("in a pessimistic scenario: € " + prettyNr (customer.pension.endamount.pessimistic) )
+			console.log ("in a average scenario: € " + prettyNr (customer.pension.endamount.average) )
+			console.log ("in a optimistic scenario: € " + prettyNr (customer.pension.endamount.optimistic) )
+		}
+
+
+	
+
